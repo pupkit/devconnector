9e6dce32b4431c6a0d77c1fef2dba27ff1d2a247
Zairi <pupkit161127@outlook.com>
2019-02-11 04:00:58 +0800

    Initial Project Setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.log("DB Connection Failed"));
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`App is running on port ${port}`));
